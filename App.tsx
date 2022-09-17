9c23ce29172e8b72de3b6b08f44fa9fb07f50da0
kstyrc <kstyrc@gmail.com>
2022-09-17 17:54:09 +0200

    resolve changes

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from "./src/views/Home";
 import Details from "./src/views/Details";
 import Submittion from "./src/views/Submittion";
+import Explore from "./src/views/Explore";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{
         headerShown: false
       }}>
+        <Stack.Screen name="Explore" component={Explore} />
         <Stack.Screen name="Home2" component={Home} />
         <Stack.Screen name="Details" component={Details} />
         <Stack.Screen name="Submittion" component={Submittion} />
