ce802022fffd273864b79b56525b2e7af0f0b6e2
kstyrc <kstyrc@gmail.com>
2022-09-17 17:52:31 +0200

    react native voice

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from "./src/views/Home";
 import Details from "./src/views/Details";
+import Explore from "./src/views/Explore";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{
         headerShown: false
       }}>
+        <Stack.Screen name="Explore" component={Explore} />
         <Stack.Screen name="Home2" component={Home} />
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
